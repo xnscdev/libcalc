63bdde4b90192fa0557f1aafb8559422ebb8310e
xnscdev <xnscdev@gmail.com>
2021-01-19 14:25:40 -0800

    Added CalcNumber class

---
+++ src/calc-number.c
@@ -0,0 +1,41 @@
+/*************************************************************************
+ * calc-number.c -- This file is part of libcalc.                        *
+ * Copyright (C) 2020 XNSC                                               *
+ *                                                                       *
+ * libcalc is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * libcalc is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+#include "calc-number.h"
+
+G_DEFINE_TYPE (CalcNumber, calc_number, CALC_TYPE_EXPR)
+
+static gboolean calc_number_equivalent (CalcExpr *self, CalcExpr *other);
+
+static void
+calc_number_class_init (CalcNumberClass *klass)
+{
+  CALC_EXPR_CLASS (klass)->equivalent = calc_number_equivalent;
+}
+
+static void
+calc_number_init (CalcNumber *self)
+{
+}
+
+static gboolean
+calc_number_equivalent (CalcExpr *self, CalcExpr *other)
+{
+  g_return_val_if_fail (CALC_IS_NUMBER (other), FALSE);
+  return mpq_equal (CALC_NUMBER (self)->value, CALC_NUMBER (other)->value);
+}
