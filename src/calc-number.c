3486fd40db3542979576e88b493a075327de76c7
xnscdev <xnscdev@gmail.com>
2021-01-20 11:09:19 -0800

    Added remaining addition functions

--- src/calc-number.c
+++ src/calc-number.c
@@ -487,6 +487,149 @@ calc_number_add_fr (CalcNumber **result, CalcNumber *a, mpfr_t b)
 }
 
 /**
+ * calc_number_add_d:
+ * @result: the pointer to store the result of the addition
+ * @a: the first addend
+ * @b: the second addend
+ *
+ * Adds @a and @b and stores the result in @result. Any previous value in
+ * @result will be erased. The type of @result is dependent on the types of
+ * @a and @b. If @result points to %NULL, a new #CalcNumber is allocated and
+ * @result will point to it. If @result is %NULL or @a is invalid, no action
+ * is performed.
+ **/
+
+void
+calc_number_add_d (CalcNumber **result, CalcNumber *a, double b)
+{
+  g_return_if_fail (result != NULL);
+  g_return_if_fail (*result == NULL || CALC_IS_NUMBER (*result));
+  g_return_if_fail (CALC_IS_NUMBER (a));
+  if (*result == NULL)
+    {
+      *result = calc_number_new (NULL);
+      mpz_clear ((*result)->integer);
+    }
+  else
+    calc_number_dispose (G_OBJECT (result));
+  (*result)->type = CALC_NUMBER_TYPE_FLOATING;
+  switch (a->type)
+    {
+    case CALC_NUMBER_TYPE_INTEGER:
+      mpfr_init_set_z ((*result)->floating, a->integer, MPFR_RNDN);
+      mpfr_add_d ((*result)->floating, (*result)->floating, b, MPFR_RNDN);
+      break;
+    case CALC_NUMBER_TYPE_RATIONAL:
+      mpfr_init_set_q ((*result)->floating, a->rational, MPFR_RNDN);
+      mpfr_add_d ((*result)->floating, (*result)->floating, b, MPFR_RNDN);
+      break;
+    case CALC_NUMBER_TYPE_FLOATING:
+      mpfr_init ((*result)->floating);
+      mpfr_add_d ((*result)->floating, a->floating, b, MPFR_RNDN);
+      break;
+    }
+}
+
+/**
+ * calc_number_add_ui:
+ * @result: the pointer to store the result of the addition
+ * @a: the first addend
+ * @b: the second addend
+ *
+ * Adds @a and @b and stores the result in @result. Any previous value in
+ * @result will be erased. The type of @result is dependent on the types of
+ * @a and @b. If @result points to %NULL, a new #CalcNumber is allocated and
+ * @result will point to it. If @result is %NULL or @a is invalid, no action
+ * is performed.
+ **/
+
+void
+calc_number_add_ui (CalcNumber **result, CalcNumber *a, unsigned long b)
+{
+  mpq_t temp;
+  g_return_if_fail (result != NULL);
+  g_return_if_fail (*result == NULL || CALC_IS_NUMBER (*result));
+  g_return_if_fail (CALC_IS_NUMBER (a));
+  if (*result == NULL)
+    {
+      *result = calc_number_new (NULL);
+      mpz_clear ((*result)->integer);
+    }
+  else
+    calc_number_dispose (G_OBJECT (result));
+  (*result)->type = a->type;
+  switch (a->type)
+    {
+    case CALC_NUMBER_TYPE_INTEGER:
+      mpz_init ((*result)->integer);
+      mpz_add_ui ((*result)->integer, a->integer, b);
+      break;
+    case CALC_NUMBER_TYPE_RATIONAL:
+      mpq_init ((*result)->rational);
+      mpq_init (temp);
+      mpq_set_ui (temp, b, 1);
+      mpq_add ((*result)->rational, a->rational, temp);
+      mpq_clear (temp);
+      break;
+    case CALC_NUMBER_TYPE_FLOATING:
+      mpfr_init ((*result)->floating);
+      mpfr_add_ui ((*result)->floating, a->floating, b, MPFR_RNDN);
+      break;
+    }
+}
+
+/**
+ * calc_number_add_si:
+ * @result: the pointer to store the result of the addition
+ * @a: the first addend
+ * @b: the second addend
+ *
+ * Adds @a and @b and stores the result in @result. Any previous value in
+ * @result will be erased. The type of @result is dependent on the types of
+ * @a and @b. If @result points to %NULL, a new #CalcNumber is allocated and
+ * @result will point to it. If @result is %NULL or @a is invalid, no action
+ * is performed.
+ **/
+
+void
+calc_number_add_si (CalcNumber **result, CalcNumber *a, signed long b)
+{
+  mpq_t temp;
+  g_return_if_fail (result != NULL);
+  g_return_if_fail (*result == NULL || CALC_IS_NUMBER (*result));
+  g_return_if_fail (CALC_IS_NUMBER (a));
+  if (*result == NULL)
+    {
+      *result = calc_number_new (NULL);
+      mpz_clear ((*result)->integer);
+    }
+  else
+    calc_number_dispose (G_OBJECT (result));
+  (*result)->type = a->type;
+  switch (a->type)
+    {
+    case CALC_NUMBER_TYPE_INTEGER:
+      mpz_init ((*result)->integer);
+      if (b >= 0)
+	mpz_add_ui ((*result)->integer, a->integer, b);
+      else
+	mpz_sub_ui ((*result)->integer, a->integer, -b);
+      break;
+    case CALC_NUMBER_TYPE_RATIONAL:
+      mpq_init ((*result)->rational);
+      mpq_init (temp);
+      mpq_set_si (temp, b, 1);
+      mpq_add ((*result)->rational, a->rational, temp);
+      mpq_clear (temp);
+      break;
+    case CALC_NUMBER_TYPE_FLOATING:
+      mpfr_init ((*result)->floating);
+      mpfr_add_si ((*result)->floating, a->floating, b, MPFR_RNDN);
+      break;
+    }
+}
+
+/**
  * calc_number_cmp:
  * @a: the first number to compare
  * @b: the second number to compare
