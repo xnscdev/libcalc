601626abefdc2f0530f47c38a2f74f0154d8295e
xnscdev <xnscdev@gmail.com>
2021-01-22 17:36:58 -0800

    Arithmetic evaluation tests

---
+++ test/eval-arith.c
@@ -0,0 +1,58 @@
+/*************************************************************************
+ * eval-arith.c -- This file is part of libcalc.                         *
+ * Copyright (C) 2020 XNSC                                               *
+ *                                                                       *
+ * libcalc is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * libcalc is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+#include <stdlib.h>
+#include "libtest.h"
+
+#define TEST_VALUE 64
+#define TEST_VARIABLE "a"
+
+DEFINE_TEST (sa_num)
+{
+  CalcNumber *a = calc_number_new_ui (TEST_VALUE);
+  CalcNumber *b = calc_number_new (NULL);
+  if (!calc_expr_evaluate (CALC_EXPR (a), CALC_EXPR (b)))
+    abort ();
+  assert_type_equals (b, CALC_NUMBER_TYPE_INTEGER);
+  assert_num_equals_ui (b, TEST_VALUE);
+  g_object_unref (a);
+  g_object_unref (b);
+}
+
+DEFINE_TEST (sa_var)
+{
+  CalcVariable *a = calc_variable_new (TEST_VARIABLE);
+  CalcNumber *b = calc_number_new_ui (TEST_VALUE);
+  CalcNumber *c = calc_number_new (NULL);
+  calc_variable_set_value (TEST_VARIABLE, CALC_EXPR (b));
+  if (!calc_expr_evaluate (CALC_EXPR (a), CALC_EXPR (c)))
+    abort ();
+  assert_num_equals_ui (c, TEST_VALUE);
+  g_object_unref (a);
+  g_object_unref (b);
+  g_object_unref (c);
+  calc_variable_set_value (TEST_VARIABLE, NULL);
+}
+
+int
+main (void)
+{
+  RUN_TEST (sa_num);
+  RUN_TEST (sa_var);
+  return 0;
+}
