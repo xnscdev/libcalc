84c85e5012766c7e97177209e734eb56b8aca91a
xnscdev <xnscdev@gmail.com>
2021-01-24 19:00:25 -0800

    Make autogen.sh compatible with autoconf < 2.70

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
 [ -z "$AUTORECONF" ] && AUTORECONF=autoreconf
+[ -z "$GTKDOCIZE" ] && GTKDOCIZE=gtkdocize
 
-$AUTORECONF --version 2>/dev/null
-if test "$?" -ne 0; then
+$AUTORECONF --version >/dev/null 2>&1
+if [ $? -ne 0 ]; then
     echo "$AUTORECONF not working or not found. Please install" 2>&1
     echo "GNU autoconf or set the AUTORECONF environment variable" 2>&1
-    echo "to an working autoreconf script." 2>&1
+    echo "to a working autoreconf script." 2>&1
+    exit 1
+fi
+
+$GTKDOCIZE --version >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "$GTKDOCIZE not working or not found. Please install" 2>&1
+    echo "Gtk-doc or set the GTKDOCIZE environment variable" 2>&1
+    echo "to a working gtkdocize script." 2>&1
     exit 1
 fi
 
 mkdir -p -v build-aux m4
+$GTKDOCIZE --copy
 $AUTORECONF --force --install --verbose
 [ -z "$NO_CONFIGURE" ] && ./configure $CONFIGURE_ARGS
